8b8f055a15af0d718028ad74dbd4848a315369b4
Vitaly Ivanov <vitaly@wickr.com>
2019-10-21 13:40:07 +0700

    turn and win/draw indicators, rendering crash fix (update textures only on the main/rendering thread)

--- movepanel.cpp
+++ movepanel.cpp
@@ -27,6 +27,12 @@ MovePanel::MovePanel(SDL_Renderer* renderer, int xOff, int width, int height):
     {
         TTF_CloseFont(_font);
         _font = TTF_OpenFont("resources/Lekton-Bold.ttf", ptSizeTarget);
+
+        surface = TTF_RenderText_Solid(_font, "Ng1-f3+", color);
+        texture = SDL_CreateTextureFromSurface(_renderer, surface);
+        SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
+        SDL_DestroyTexture(texture);
+        SDL_FreeSurface(surface);
     }
 
     _backgroundRect.x = xOff;
@@ -39,6 +45,19 @@ MovePanel::MovePanel(SDL_Renderer* renderer, int xOff, int width, int height):
     _movesTextureRect[Black].x = _movesTextureRect[White].x + _backgroundRect.w / 2;
     _movesTextureRect[Black].y = _movesTextureRect[White].y;
 
+    _winDrawIndicatorTextureRect.x = _movesTextureRect[White].x;
+    _winDrawIndicatorTextureRect.y = _backgroundRect.h - texH - _backgroundRect.w / 32;
+
+    _turnIndicatorTextureRect[White].x = _turnIndicatorTextureRect[Black].x = _movesTextureRect[White].x;
+    _turnIndicatorTextureRect[White].y = _turnIndicatorTextureRect[Black].y = _backgroundRect.w / 32;
+
+    SDL_Color colors[] = {{ 255, 255, 255, 255 }, { 0, 0, 0, 255 }};
+    for (int c = White; c <= Black; ++c)
+    {
+        SDL_Surface* surface = TTF_RenderText_Blended_Wrapped(_font, (c ? "Black turn" : "White turn"), colors[c], _backgroundRect.w);
+        UpdateTexture(surface, &_turnIndicatorTexture[c], &_turnIndicatorTextureRect[c]);
+    }
+
     _position.setInitial();
 }
 
@@ -48,7 +67,13 @@ MovePanel::~MovePanel()
     {
         if (_movesTexture[c])
             SDL_DestroyTexture(_movesTexture[c]);
+
+        if (_turnIndicatorTexture[c])
+            SDL_DestroyTexture(_turnIndicatorTexture[c]);
     }
+
+    if (_winDrawIndicatorTexture)
+        SDL_DestroyTexture(_winDrawIndicatorTexture);
 
     if (_font)
         TTF_CloseFont(_font);
@@ -64,7 +89,7 @@ void MovePanel::SetPosition(const fatpup::Position& pos)
     }
 
     _position = pos;
-    PositionUpdated();
+    _positionUpdated = true;
 }
 
 void MovePanel::Move(fatpup::Move move)
@@ -73,52 +98,65 @@ void MovePanel::Move(fatpup::Move move)
     _movesString[c] += _position.moveToString(move) + "\n";
 
     _position += move;
-    PositionUpdated();
+    _positionUpdated = true;
 }
 
-void MovePanel::PositionUpdated()
+void MovePanel::UpdateTexture(SDL_Surface* surface, SDL_Texture** texture, SDL_Rect* textureRect)
 {
+    if (*texture)
+        SDL_DestroyTexture(*texture), *texture = nullptr;
+
+    if (surface)
+    {
+        *texture = SDL_CreateTextureFromSurface(_renderer, surface);
+        SDL_FreeSurface(surface);
+    }
+
+    if (*texture)
+    {
+        int texW = 0, texH = 0;
+        SDL_QueryTexture(*texture, NULL, NULL, &texW, &texH);
+        textureRect->w = texW;
+        textureRect->h = texH;
+    }
+}
+
+void MovePanel::UpdateTextures()
+{
+    if (!_positionUpdated)
+        return;
+
+    _positionUpdated = false;
+
     _positionState = _position.getState();
-/*
-    if (_positionState == fatpup::Position::State::Checkmate)
-    {
-        _winDrawIndicator.setString(std::string(_position.isWhiteTurn() ? "Black" : "White") +  " won");
-        _winDrawIndicator.setFillColor(_position.isWhiteTurn() ? sf::Color::Black : sf::Color::White);
-    }
-    else if (_positionState == fatpup::Position::State::Stalemate)
-    {
-        _winDrawIndicator.setString("Draw");
-        _winDrawIndicator.setFillColor(sf::Color(128, 128, 128));
-    }
-*/
 
     if (_font)
     {
-        SDL_Color colors[] = {{ 255, 255, 255, 255 }, { 0, 0, 0, 255 }};
+        static const SDL_Color colors[] = {{ 255, 255, 255, 255 }, { 0, 0, 0, 255 }};
+
+        if (_positionState == fatpup::Position::State::Checkmate)
+        {
+            const auto c = _position.isWhiteTurn() ? White : Black;
+            const std::string winText(std::string(c ? "Black" : "White") + " won");
+            SDL_Surface* surface = TTF_RenderText_Blended(_font, winText.c_str(), colors[c]);
+            UpdateTexture(surface, &_winDrawIndicatorTexture, &_winDrawIndicatorTextureRect);
+        }
+        else if (_positionState == fatpup::Position::State::Stalemate)
+        {
+            const SDL_Color gray = { 128, 128, 128, 255 };
+            SDL_Surface* surface = TTF_RenderText_Blended(_font, "Draw", gray);
+            UpdateTexture(surface, &_winDrawIndicatorTexture, &_winDrawIndicatorTextureRect);
+        }
+
         for (int c = White; c <= Black; ++c)
         {
             SDL_Surface* surface = TTF_RenderText_Blended_Wrapped(_font, _movesString[c].c_str(), colors[c], _backgroundRect.w / 2);
-            if (surface)
-            {
-                if (_movesTexture[c])
-                    SDL_DestroyTexture(_movesTexture[c]), _movesTexture[c] = nullptr;
-
-                _movesTexture[c] = SDL_CreateTextureFromSurface(_renderer, surface);
-                SDL_FreeSurface(surface);
-
-                if (_movesTexture[c])
-                {
-                    int texW = 0, texH = 0;
-                    SDL_QueryTexture(_movesTexture[c], NULL, NULL, &texW, &texH);
-                    _movesTextureRect[c].w = texW;
-                    _movesTextureRect[c].h = texH;
-                }
-            }
+            UpdateTexture(surface, &_movesTexture[c], &_movesTextureRect[c]);
         }
     }
 }
 
-void MovePanel::Render() const
+void MovePanel::Render()
 {
     static const uint8_t backgroundColor[] =
     {
@@ -129,19 +167,24 @@ void MovePanel::Render() const
     SDL_SetRenderDrawColor(_renderer, backgroundColor[0], backgroundColor[1], backgroundColor[2], 255);
     SDL_RenderFillRect(_renderer, &_backgroundRect);
 
+    UpdateTextures();
+
     for (int c = White; c <= Black; ++c)
     {
         if (_movesTexture[c])
             SDL_RenderCopy(_renderer, _movesTexture[c], NULL, &_movesTextureRect[c]);
     }
-/*
-    window.draw(_background);
+
     if (_positionState != fatpup::Position::State::Checkmate && _positionState != fatpup::Position::State::Stalemate)
-        window.draw(_turnIndicator[_position.isWhiteTurn() ? White : Black]);
-    window.draw(_moves[White]);
-    window.draw(_moves[Black]);
+    {
+        const auto c = _position.isWhiteTurn() ? White : Black;
+        if (_turnIndicatorTexture[c])
+            SDL_RenderCopy(_renderer, _turnIndicatorTexture[c], NULL, &_turnIndicatorTextureRect[c]);
+    }
 
     if (_positionState == fatpup::Position::State::Checkmate || _positionState == fatpup::Position::State::Stalemate)
-        window.draw(_winDrawIndicator);
-*/
+    {
+        if (_winDrawIndicatorTexture)
+            SDL_RenderCopy(_renderer, _winDrawIndicatorTexture, NULL, &_winDrawIndicatorTextureRect);
+    }
 }
--- movepanel.h
+++ movepanel.h
@@ -18,15 +18,17 @@ public:
     void SetPosition(const fatpup::Position& pos);
     void Move(fatpup::Move move);
 
-    void Render() const;
+    void Render();
 
 private:
-    void PositionUpdated();
+    void UpdateTextures();
+    void UpdateTexture(SDL_Surface* surface, SDL_Texture** texture, SDL_Rect* textureRect); // releases surface too
 
     SDL_Renderer* _renderer = nullptr;
 
     fatpup::Position _position;
     fatpup::Position::State _positionState;
+    bool _positionUpdated = true;
 
     SDL_Rect _backgroundRect;
     TTF_Font* _font = nullptr;
@@ -39,6 +41,12 @@ private:
     std::string _movesString[2];
     SDL_Texture* _movesTexture[2] = { nullptr, nullptr };
     SDL_Rect _movesTextureRect[2];
+
+    SDL_Texture* _turnIndicatorTexture[2] = { nullptr, nullptr };
+    SDL_Rect _turnIndicatorTextureRect[2];
+
+    SDL_Texture* _winDrawIndicatorTexture = nullptr;
+    SDL_Rect _winDrawIndicatorTextureRect;
 };
 
 #endif // #define FATPUP_UI_MOVEPANEL_H
